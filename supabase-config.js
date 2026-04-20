7d30f14908efa6e336e55166998d3abea8fae972
Emad <emeduq@gmail.com>
2026-04-20 07:57:17 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,4 @@
-// استيراد العميل من مكتبة Supabase
-const { createClient } = window.supabase;
+const supabaseUrl = 'https://qgbuvchbhbtkwcvafhek.supabase.co';
+const supabaseKey = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnYnV2Y2hiaGJ0a3djdmFmaGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MTI0ODksImV4cCI6MjA5MjE4ODQ4OX0.rBc4KTn0c8OhhG2dFD9ZLDHknBhhOllYsxBMzuvWeCY';
 
-// البيانات التي استخرجناها من مشروعك
-const supabaseUrl = 'https://qgbuvchbhbtkwcvafhek.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnYnV2Y2hiaGJ0a3djdmFmaGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MTI0ODksImV4cCI6MjA5MjE4ODQ4OX0.rBc4KTn0c8OhhG2dFD9ZLDHknBhhOllYsxBMzuvWeCY';
-
-// إنشاء الاتصال وتصديره
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
