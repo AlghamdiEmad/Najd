5c015d205239ec9585ea7933a4f79d9b1afbfedd
Emad <emeduq@gmail.com>
2026-04-21 19:18:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
-self.addEventListener('fetch', function(event) {
-  // هذا الكود ضروري ليتم اعتبار الموقع PWA
+// هذا الكود فقط لإثبات وجود "موظف" شغال للمتصفح
+self.addEventListener('install', (event) => {
+    console.log('Najd Service Worker Installed');
 });
 
+self.addEventListener('fetch', (event) => {
+    // يترك فارغاً حالياً، فقط للوفاء بشروط التثبيت
+});
